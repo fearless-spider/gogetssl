58b9d3876a6adf7f79a3c5ae8ad98e0c859f0ff3
Przemyslaw Pajak <office@fearlessspider.com>
2024-01-13 19:57:01 +0100

    initial

---
+++ lib/gogetssl.ex
@@ -0,0 +1,18 @@
+defmodule Gogetssl do
+  @moduledoc """
+  Documentation for `Gogetssl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gogetssl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gogetssl_test.exs
@@ -0,0 +1,8 @@
+defmodule GogetsslTest do
+  use ExUnit.Case
+  doctest Gogetssl
+
+  test "greets the world" do
+    assert Gogetssl.hello() == :world
+  end
+end
