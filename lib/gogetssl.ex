a58bb295705154df34dfb10794b7537b6dfe3985
Przemyslaw Pajak <office@fearlessspider.com>
2024-01-13 22:44:02 +0100

    add initial code for api endpoint

--- lib/gogetssl.ex
+++ lib/gogetssl.ex
@@ -1,18 +1,49 @@
 defmodule Gogetssl do
   @moduledoc """
-  Documentation for `Gogetssl`.
+  Main module for handling sending/receiving requests to GoGetSSL's API
   """
 
-  @doc """
-  Hello world.
+  @default_api_endpoint "https://my.gogetssl.com/api/"
+  @client_version Mix.Project.config()[:version]
 
-  ## Examples
+  def version do
+    @client_version
+  end
 
-      iex> Gogetssl.hello()
-      :world
+  @missing_username_password_error_message """
+    The ggssl_auth_username and the ggssl_auth_password settings is required to use gogetssl. Please include your
+    gogetss username and password in your application config file like so:
 
+    config :gogetssl, ggssl_auth_username: {:system, "GGSSL_AUTH_USERNAME"}, ggssl_auth_password: {:system, "GGSSL_AUTH_PASSWORD"}
   """
-  def hello do
-    :world
+
+  defp get_username do
+    System.get_env("GGSSL_AUTH_USERNAME") ||
+      Application.get_env(:gogetssl, :ggssl_auth_username) ||
+      raise AuthenticationError, message: @missing_username_password_error_message
+  end
+
+  defp get_password do
+    System.get_env("GGSSL_AUTH_PASSWORD") ||
+      Application.get_env(:gogetssl, :ggssl_auth_password) ||
+      raise AuthenticationError, message: @missing_username_password_error_message
+  end
+
+  defp get_api_endpoint do
+    System.get_env("GOGETSSL_API_ENDPOINT") ||
+      Application.get_env(:stripe, :api_endpoint) ||
+      @default_api_endpoint
+  end
+
+  defp request_url(endpoint) do
+    Path.join(get_api_endpoint(), endpoint)
+  end
+
+  defp request_url(endpoint, []) do
+    Path.join(get_api_endpoint(), endpoint)
+  end
+
+  defp process_response_body(body) do
+    Poison.decode!(body)
   end
 end
--- test/gogetssl_test.exs
+++ test/gogetssl_test.exs
@@ -1,8 +1,4 @@
 defmodule GogetsslTest do
   use ExUnit.Case
   doctest Gogetssl
-
-  test "greets the world" do
-    assert Gogetssl.hello() == :world
-  end
 end
