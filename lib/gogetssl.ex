43b3e1c9d0b2d949b7f10f450ad61ea3542a45ff
Przemyslaw Pajak <office@fearlessspider.com>
2024-01-14 13:26:15 +0100

    add request

--- lib/gogetssl.ex
+++ lib/gogetssl.ex
@@ -10,7 +10,7 @@ defmodule Gogetssl do
     @client_version
   end
 
-  alias Gogetssl.{AuthenticationError}
+  alias Gogetssl.{AuthenticationError, APIConnectionError, APIError, InvalidRequestError}
 
   @missing_username_password_error_message """
     The ggssl_auth_username and the ggssl_auth_password settings is required to use gogetssl. Please include your
@@ -45,6 +45,44 @@ defmodule Gogetssl do
     Path.join(get_api_endpoint(), endpoint)
   end
 
+  defp request_url(endpoint, data, auth_key) do
+    base_url = request_url(endpoint)
+    query_params = Gogetssl.Utils.encode_data(data)
+    "#{base_url}?auth_key=#{auth_key}"
+  end
+
+  def request(action, endpoint, data, opts) when action in [:get, :post, :delete] do
+    HTTPoison.request(action, request_url(endpoint, data), "")
+    |> handle_response
+  end
+
+  defp handle_response({:ok, %{body: body, status_code: 200}}) do
+    {:ok, process_response_body(body)}
+  end
+  
+  defp handle_response({:ok, %{body: body, status_code: code}}) do
+    %{"message" => message} = error =
+      body
+      |> process_response_body
+      |> Map.fetch!("error")
+
+    error_struct =
+      case code do
+        code when code in [400, 404] ->
+          %InvalidRequestError{message: message, param: error["param"]}
+        401 ->
+          %AuthenticationError{message: message}
+        _ ->
+          %APIError{message: message}
+      end
+
+    {:error, error_struct}
+  end
+
+  defp handle_response({:error, %HTTPoison.Error{reason: reason}}) do
+    %APIConnectionError{message: "Network Error: #{reason}"}
+  end
+  
   defp process_response_body(body) do
     Poison.decode!(body)
   end
