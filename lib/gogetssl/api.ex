8eda3fb12c55a2e85750765889d7bbc979a8e9e0
Przemysław Pająk <office@fearlessspider.com>
2024-01-15 15:16:39 +0100

    add API module

---
+++ lib/gogetssl/api.ex
@@ -0,0 +1,53 @@
+defmodule Gogetssl.API do
+  defmacro __using__(opts) do
+    quote do
+      if :create in unquote(opts) do
+        @doc """
+        Create a(n) #{__MODULE__ |> to_string |> String.split(".") |> List.last}
+        """
+        def create(data, opts \\ []) do
+          Gogetssl.request(:post, endpoint(), data, opts)
+        end
+      end
+
+      if :retrieve in unquote(opts) do
+        @doc """
+        Retrive a(n) #{__MODULE__ |> to_string |> String.split(".") |> List.last} by its ID
+        """
+        def retrieve(id, opts \\ []) when is_bitstring(id) do
+          resource_url = Path.join(endpoint(), id)
+          Gogetssl.request(:get, resource_url, [], opts)
+        end
+      end
+
+      if :update in unquote(opts) do
+        @doc """
+        Update a(n) #{__MODULE__ |> to_string |> String.split(".") |> List.last}
+        """
+        def update(id, data, opts \\ []) when is_bitstring(id) do
+          resource_url = Path.join(endpoint(), id)
+          Gogetssl.request(:post, resource_url, data, opts)
+        end
+      end
+
+      if :list in unquote(opts) do
+        @doc """
+        List all #{__MODULE__ |> to_string |> String.split(".") |> List.last}s
+        """
+        def list(pagination_opts \\ [], opts \\ []) when is_list(pagination_opts) do
+          Gogetssl.request(:get, endpoint(), pagination_opts, opts)
+        end
+      end
+
+      if :delete in unquote(opts) do
+        @doc """
+        Delete a(n) #{__MODULE__ |> to_string |> String.split(".") |> List.last}
+        """
+        def delete(id, data \\ [], opts \\ []) when is_bitstring(id) do
+          resource_url = Path.join(endpoint(), id)
+          Gogetssl.request(:delete, resource_url, data, opts)
+        end
+      end
+    end
+  end
+end
